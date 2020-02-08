60178b184d876f56ccec259178d6ff664285d06f
sonkkeli <laurilasonja@gmail.com>
2020-02-08 15:37:15 +0200

    changed the server implementation

--- src/main/java/packageviewer/Server.java
+++ src/main/java/packageviewer/Server.java
@@ -1,9 +1,11 @@
 package packageviewer;
 
 import com.alibaba.fastjson.JSON;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.*;
-import java.nio.file.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,60 +23,54 @@ public class Server {
     public Server() {        
     }
     
-    public void openConnection(Map<String, PackageObject> p) throws Exception{
-        int port = Integer.parseInt(System.getenv("PORT"));
-        this.server = new ServerSocket(port);
+    public void openConnection (Map<String, PackageObject> p) throws Exception {
+        int port = Integer.parseInt(System.getenv("PORT")); // running in heroku
+//        int port = 8081; // running locally
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         this.packages = p;
+        
+        HttpContext context = server.createContext("/api/packages", (exchange -> {                  
+            String respText = JSON.toJSONString(packages.keySet().stream().sorted().collect(Collectors.toList()));            
+          
+            Headers h = exchange.getResponseHeaders();
+            h.add("Access-Control-Allow-Origin", "*");
+            h.add("Content-Type", "application/json");
+            
+            exchange.sendResponseHeaders(200, respText.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
 
-        while (true) {
-            this.socket = server.accept();
-            scanner = new Scanner(socket.getInputStream());
-            String url = "";
-            if(scanner.hasNextLine()){
-                url = scanner.nextLine();
-                System.out.println(url);
-            }            
+            output.write(respText.getBytes());
+            output.flush();
+            exchange.close();
+        }));
+        
+        HttpContext context2 = server.createContext("/api/package/", (exchange -> {                  
+            URI test = exchange.getRequestURI();
+            String param = getQueryParam(test.toString());
             
-            if(url.contains("/closeconnectionbutdontbetooobvious")) break;
-            
-            if(!url.isEmpty() && url.contains("/api/package/")){
-                String packageName = url.split("/")[3].split("HTTP")[0].trim();
+            String respText = JSON.toJSONString(packages.getOrDefault(param, null));
+            if(!respText.equals("null")){
+                Headers h = exchange.getResponseHeaders();
+                h.add("Access-Control-Allow-Origin", "*");
+                h.add("Content-Type", "application/json");            
+                exchange.sendResponseHeaders(200, respText.getBytes().length);
                 
-                if(packages.get(packageName) != null){
-                    this.pw = new PrintWriter(socket.getOutputStream());
-                    pw.println("HTTP/1.1 200 OK");
-                    pw.println("Access-Control-Allow-Origin: *");
-                    pw.println("Content-Type: application/json");
-                    pw.println("");                    
-                    String json = JSON.toJSONString(packages.get(packageName));
-                    pw.println(json);
-                } else {
-                    pw.println("HTTP/1.1 404");
-                    pw.println("");
-                }
-                pw.flush();
-                
-            } else if(!url.isEmpty() && url.contains("/api/packages")){
-                this.pw = new PrintWriter(socket.getOutputStream());
-                pw.println("HTTP/1.1 200 OK");
-                pw.println("Access-Control-Allow-Origin: *");
-                pw.println("Content-Type: application/json");
-                pw.println("");
-                String json = JSON.toJSONString(packages.keySet().stream().sorted().collect(Collectors.toList()));
-                pw.println(json);
-                pw.flush();
-                
+                OutputStream output = exchange.getResponseBody();
+                output.write(respText.getBytes());
+                output.flush();
             } else {
-                this.pw = new PrintWriter(socket.getOutputStream());
-                pw.println("HTTP/1.1 200 OK");
-                pw.println("");
-                Files.lines(Paths.get("index.html"))
-                    .forEach(line -> pw.println(line));
-                pw.flush();
-            }
-            pw.close();            
-        }
-        socket.close();
-        scanner.close();
+                exchange.sendResponseHeaders(404, -1);// 404 Not found
+            }            
+            exchange.close();
+        }));
+        
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+    
+    private String getQueryParam(String url){
+        String[] parts = url.split("/");
+        System.out.println(parts[3]);
+        return parts[3];
     }
 }
